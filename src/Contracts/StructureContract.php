58478a58e0be2d1fc11458a45fc695bad83d4616
Mark Jaquith <mark@jaquith.me>
2021-03-09 12:54:37 -0500

    Bulk up the StructureContract interface

--- src/Contracts/StructureContract.php
+++ src/Contracts/StructureContract.php
@@ -3,4 +3,8 @@
 namespace MarkJaquith\Wherewithal\Contracts;
 
 interface StructureContract {
+	public function __toString(): string;
+	public function toString(): string;
+	public function getBindings(): array;
+	public function toArray(): array;
 }
